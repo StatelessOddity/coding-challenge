c23011247f86e147f23911a4ade25c83b93310d3
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-01-16 16:14:43 +0100

    test

--- security.tf
+++ security.tf
@@ -4,14 +4,25 @@ resource "aws_security_group" "rdx_public_security_group" {
   vpc_id = aws_vpc.rdx_vpc.id
 }
 
-resource "aws_security_group_rule" "rdx_public_security_group_rules" {
-  count = length(var.public_security_group_rules)
+resource "aws_security_group_rule" "rdx_public_security_group_rules_ingress" {
+  count = length(var.public_security_group_rules_ingress)
   type              = "ingress"
-  from_port         = var.public_security_group_rules[count.index].from_port
-  to_port           = var.public_security_group_rules[count.index].to_port
-  protocol          = var.public_security_group_rules[count.index].protocol
-  cidr_blocks       = [var.public_security_group_rules[count.index].cidr_block]
-  description       = var.public_security_group_rules[count.index].description
+  from_port         = var.public_security_group_rules_ingress[count.index].from_port
+  to_port           = var.public_security_group_rules_ingress[count.index].to_port
+  protocol          = var.public_security_group_rules_ingress[count.index].protocol
+  cidr_blocks       = [var.public_security_group_rules_ingress[count.index].cidr_block]
+  description       = var.public_security_group_rules_ingress[count.index].description
+  security_group_id = aws_security_group.rdx_public_security_group.id
+}
+
+resource "aws_security_group_rule" "rdx_public_security_group_rules_engress" {
+  count = length(var.public_security_group_rules_engress)
+  type              = "engress"
+  from_port         = var.public_security_group_rules_engress[count.index].from_port
+  to_port           = var.public_security_group_rules_engress[count.index].to_port
+  protocol          = var.public_security_group_rules_engress[count.index].protocol
+  cidr_blocks       = [var.public_security_group_rules_engress[count.index].cidr_block]
+  description       = var.public_security_group_rules_engress[count.index].description
   security_group_id = aws_security_group.rdx_public_security_group.id
 }
 
