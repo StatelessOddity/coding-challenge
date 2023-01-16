1f24a1cf05184b94f2d3bf60a8616b5c174ba7e3
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-01-16 22:42:13 +0100

    test

--- github.tf
+++ github.tf
@@ -2,5 +2,5 @@ resource "github_actions_secret" "example_secret" {
   count = var.full_node_ec2.count
   repository       = "coding-challenge"
   secret_name      = "IP"
-  plaintext_value  = aws_instance.full_node_ec2[count.index].public_ip
+  plaintext_value  = aws_eip.fullnode_ips[count.index].public_ip
 }
