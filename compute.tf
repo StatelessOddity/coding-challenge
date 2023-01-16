c29a0812ebbdbb3af9afc261026e262b805ca5f3
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-01-16 14:09:24 +0100

    test

---
+++ compute.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "full_node_ec2" {
+  count = var.full_node_ec2.count
+  ami = data.aws_ami.ubuntu.id
+  instance_type = var.full_node_ec2.instance_type
+  subnet_id = aws_subnet.rdx_public_subnet[count.index].id
+  vpc_security_group_ids = [aws_security_group.rdx_public_security_group.id]
+  
+  tags = {
+    Name = "RDX Full Node ${count.index}"
+  }
+}
