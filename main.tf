e07a8497b0153e8252a76fcd566e7f202a29da21
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-01-16 09:58:06 +0100

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/* 
+This setup is pretty straightforward, I'm not creating any custom modules.
+My goal was to prepare something similar to how production setup MIGHT look like for the client.
+Of course I skipped redundancy and scaled it down/simplified to save costs :)
+*/
+
 resource "aws_vpc" "rdx_vpc" {
   cidr_block = var.rdx_vpc_cidr_block
   enable_dns_hostnames = true
@@ -5,8 +11,6 @@ resource "aws_vpc" "rdx_vpc" {
     "Name" = "RDX VPC"
   }
 }
-
-/*
 
 resource "aws_internet_gateway" "rdx_internet_gateway" {
   vpc_id = aws_vpc.rdx_vpc.id
@@ -21,7 +25,7 @@ resource "aws_subnet" "rdx_public_subnet" {
   cidr_block = var.rdx_public_subnet_cidr_blocks[count.index]
   availability_zone = data.aws_avalibility_zones.avalible.names[count.index]
   tags = {
-    Name = "RDX public subnet"
+    Name = "RDX public subnet $(count.index)"
   }
 }
 
@@ -35,8 +39,91 @@ resource "aws_subnet" "rdx_private_subnet" {
   }
 }
 
-resource "github_action_secret" "example_secret" {
-  
+resource "aws_route_table" "rdx_public_route_table" {
+  vpc_id = aws_vpc.rdx_vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.rdx_internet_gateway.id
+  }
 }
 
-*/
+resource "aws_route_table_association" "public" {
+  count = var.rdx_vpc_subnet_count.public
+  route_table_id = aws_route_table.rdx_public_route_table.id
+  subnet_id = aws_subnet.rdx_public_subnet[count.index].id
+}
+
+resource "aws_route_table" "rdx_private_route_table" {
+  vpc_id = aws_vpc.rdx_vpc.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = var.rdx_vpc_subnet_count.private
+  route_table_id = aws_route_table.rdx_private_route_table.id
+  subnet_id = aws_subnet.rdx_private_subnet[count.index].id
+}
+
+resource "aws_security_group" "rdx_public_security_group" {
+  name = "rdx_public_security_group"
+  description = "Security for compute EC2 instances of Full Node, Gateway and monitoring"
+  vpc_id = aws_vpc.rdx_vpc.id
+}
+
+resource "aws_security_group_rule" "rdx_public_security_group_rules" {
+  count = length(var.public_security_group_rules)
+
+  type              = "ingress"
+  from_port         = var.public_security_group_rules[count.index].from_port
+  to_port           = var.public_security_group_rules[count.index].to_port
+  protocol          = var.public_security_group_rules[count.index].protocol
+  cidr_blocks       = [var.public_security_group_rules[count.index].cidr_block]
+  description       = var.public_security_group_rules[count.index].description
+  security_group_id = aws_security_group.rdx_public_security_group.id
+}
+
+resource "aws_security_group" "rdx_private_security_group" {
+  name = "rdx_private_security_group"
+  description = "security group for the Gateway API PostgreSQL Database"
+  vpc_id = aws_vpc.rdx_vpc
+
+  ingress = [ {
+    description = "Allow PostgreSQL traffic fom Node and Gateway"
+    from_port = 5432
+    protocol = "tcp"
+    security_groups = [ aws_security_group.rdx_public_security_group ]
+    to_port = 5432
+  } ]
+}
+
+resource "aws_db_subnet_group" "rdx_private_subnet_group" {
+  name = "rdx_private_subnet_group"
+  description = "Subnet group for the PostreSQL database"
+  subnet_ids = [for subnet in aws_subnet.rdx_private_subnet : subnet.id]
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = "true"
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hmv"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "full_node_ec2" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = var.full_node_ec2.instance_type
+  subnet_id = aws_subnet.rdx_public_subnet[count.index].id
+  vpc_security_group_ids = [aws_security_group.rdx_public_security_group.id]
+  
+  tags = {
+    Name = "RDX Full Node"
+  }
+}
