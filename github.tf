84805e3db4afb044bfffad84902d9263a1500606
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-01-16 22:14:27 +0100

    test

---
+++ github.tf
@@ -0,0 +1,5 @@
+resource "github_actions_secret" "example_secret" {
+  repository       = "coding-challenge"
+  secret_name      = "IP"
+  plaintext_value  = aws_instance.full_node_ec2[count.index].public_ip
+}
