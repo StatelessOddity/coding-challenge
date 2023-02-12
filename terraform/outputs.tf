0c0615d1c388ee84d3bb164d4cffc0f3ac619f76
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-02-12 13:28:23 +0100

    fullnode ip output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "fullnode_ec2_public_IP" {
   value = aws_eip.fullnode_ip[count.index].public_ip
 }
-
-output "fullnode_eip_public_IP" {
-  value = aws_instance.full_node_ec2[count.index].public_ip
-}
