9e6026fb7f79d77fcd7547da107897529536c7ef
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-02-13 18:59:10 +0100

    test

--- terraform/data.tf
+++ terraform/data.tf
@@ -20,11 +20,10 @@ data "aws_availability_zones" "available" {
 }
 
 data "template_file" "ansible_inventory" {
-  content = templatefile("${path.module}/templates/hosts.tpl",
-    {
-      fullnode_ips = aws_eip.fullnode_ip.*.public_ip
+  template = "${file("./templates/hosts.tpl")}"
+    vars  {
+      fullnode_ips        = aws_eip.fullnode_ip.*.public_ip
       data_aggregator_ips = aws_eip.data_aggregator_ip.*.public_ip
-      gateway_api_ips = aws_eip.gateway_api_ip.*.public_ip
+      gateway_api_ips     = aws_eip.gateway_api_ip.*.public_ip
     }
-  )
 }
