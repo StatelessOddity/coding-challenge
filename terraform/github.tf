5feff47d47a04eb96ab360c40961af8a63215b9a
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-02-14 12:31:46 +0100

    test

--- terraform/github.tf
+++ terraform/github.tf
@@ -4,3 +4,9 @@ resource "github_actions_secret" "fullnode_external_ip" {
   secret_name     = "IP"
   plaintext_value = aws_eip.fullnode_ip[count.index].public_ip
 }
+
+resource "github_actions_secret" "ansible_inventory" {
+  repository      = "coding-challenge"
+  secret_name     = "ANSIBLE_INVENTORY"
+  plaintext_value = data.template_file.ansible_inventory.rendered
+}
