95a5f07e94a9851ceb097b1a7d2b53e005a00d20
StatelessOddity <120978900+StatelessOddity@users.noreply.github.com>
2023-03-03 17:30:50 +0100

    tteesst

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "bastion" {
-  count                  = var.bastion.count
-  ami                    = "ami-005b11f8b84489615"
-  instance_type          = var.bastion.instance_type
-  subnet_id              = aws_subnet.rdx_stack[count.index].id
-  vpc_security_group_ids = [aws_security_group.rdx_stack.id]
-  key_name               = aws_key_pair.rdx_key.key_name
+  count                       = var.bastion.count
+  ami                         = "ami-005b11f8b84489615"
+  instance_type               = var.bastion.instance_type
+  subnet_id                   = aws_subnet.rdx_stack[count.index].id
+  vpc_security_group_ids      = [aws_security_group.rdx_stack.id]
+  key_name                    = aws_key_pair.rdx_key.key_name
   associate_public_ip_address = var.bastion.public_ip
   tags = {
     Name = "Bastion Desktop ${count.index + 1}"
@@ -32,12 +32,12 @@ resource "aws_instance" "full_node" {
 }
 
 resource "aws_instance" "data_aggregator" {
-  count                  = var.data_aggregator.count
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.data_aggregator.instance_type
-  subnet_id              = aws_subnet.rdx_stack[count.index].id
-  vpc_security_group_ids = [aws_security_group.rdx_stack.id]
-  key_name               = aws_key_pair.rdx_key.key_name
+  count                       = var.data_aggregator.count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.data_aggregator.instance_type
+  subnet_id                   = aws_subnet.rdx_stack[count.index].id
+  vpc_security_group_ids      = [aws_security_group.rdx_stack.id]
+  key_name                    = aws_key_pair.rdx_key.key_name
   associate_public_ip_address = var.data_aggregator.public_ip
   tags = {
     Name = "Data Aggregator ${count.index + 1}"
@@ -45,12 +45,12 @@ resource "aws_instance" "data_aggregator" {
 }
 
 resource "aws_instance" "gateway_api" {
-  count                  = var.gateway_api.count
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.gateway_api.instance_type
-  subnet_id              = aws_subnet.rdx_stack[count.index].id
-  vpc_security_group_ids = [aws_security_group.rdx_stack.id]
-  key_name               = aws_key_pair.rdx_key.key_name
+  count                       = var.gateway_api.count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.gateway_api.instance_type
+  subnet_id                   = aws_subnet.rdx_stack[count.index].id
+  vpc_security_group_ids      = [aws_security_group.rdx_stack.id]
+  key_name                    = aws_key_pair.rdx_key.key_name
   associate_public_ip_address = var.gateway_api.public_ip
   tags = {
     Name = "Gateway API ${count.index + 1}"
@@ -58,12 +58,12 @@ resource "aws_instance" "gateway_api" {
 }
 
 resource "aws_instance" "monitoring" {
-  count                  = var.monitoring.count
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.monitoring.instance_type
-  subnet_id              = aws_subnet.rdx_stack[count.index].id
-  vpc_security_group_ids = [aws_security_group.rdx_stack.id]
-  key_name               = aws_key_pair.rdx_key.key_name
+  count                       = var.monitoring.count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.monitoring.instance_type
+  subnet_id                   = aws_subnet.rdx_stack[count.index].id
+  vpc_security_group_ids      = [aws_security_group.rdx_stack.id]
+  key_name                    = aws_key_pair.rdx_key.key_name
   associate_public_ip_address = var.monitoring.public_ip
   tags = {
     Name = "Monitoring ${count.index + 1}"
